1fac2cb8660e74cd8f74de83165f0ac9577f01d0
ZhYuan <943497030@qq.com>
2018-12-09 15:34:11 +0800

    Finish layout and routes

---
+++ resources/views/layouts/_header.blade.php
@@ -0,0 +1,17 @@
+<header class="">
+    <div class="container">
+        <div class="row">
+            <div class="navbar col-sm-9">
+                <a class="navbar-brand" href="{{ url('/') }}" id="logo">Sample App</a>
+            </div>
+            <div class="col-sm-3">
+                <nav class="navbar navbar-expand-lg">
+                    <ul class="navbar-nav pull-right">
+                        <li class="nav-item nav-link"><a href="#">登录</a></li>
+                        <li class="nav-item nav-link"><a href="{{ route('help') }}">帮助</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </div>
+</header>
--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,11 @@
 |
 */
 
-Route::get('/', 'StaticPagesController@home');
+Route::get('/', 'StaticPagesController@home')->name('home');
 
-Route::get('/help', 'StaticPagesController@help');
+Route::get('/help', 'StaticPagesController@help')->name('help');
 
-Route::get('/about', 'StaticPagesController@about');
+Route::get('/about', 'StaticPagesController@about')->name('about');
+
+/*注册路由*/
+Route::get('sign','UsersController@create')->name('sign');
